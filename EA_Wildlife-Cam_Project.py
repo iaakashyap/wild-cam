5d7df2e842ce3dcb0a3fbb7e1dd3ea81b8ad7cb6
Anika_Kashyap <akashyap@ideaventionsacademy.org>
2018-03-13 15:35:08 +0000

    adding cam project code

---
+++ EA_Wildlife-Cam_Project.py
@@ -0,0 +1,30 @@
+# Wildlife camera Project Code
+# Made by Erin and Anika
+# Start Date: 8:48am 3/13/2018 End Date:
+# This project is to make a camera that will take pictures of wildlife.
+
+# Import libraies
+# For Camera and button 
+from picamera import PiCamera
+from gpiozero import Button
+# For LEDs
+import time
+import RPi.GPIO as GPIO
+
+# Button and On/off LED
+
+
+
+
+
+
+
+# Infa LEDs
+
+
+
+
+
+
+
+# PIR and Camera
